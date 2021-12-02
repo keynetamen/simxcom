898581bd4af4fb469711d6c6942f00416aa5bc12
Keyne Tamen <keynetamen@gmail.com>
2021-12-03 00:59:41 +0100

    Add get_active_window()

--- src/simxcom.c
+++ src/simxcom.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <X11/Xatom.h>
 #include <X11/Xlib.h>
 
+Window get_active_window(Display *dpy, Window root)
+{
+    Atom prop = XInternAtom(dpy, "_NET_ACTIVE_WINDOW", True), type;
+    Window active;
+    int format;
+    unsigned long len, extra;
+    unsigned char *result = NULL;
+
+    if(XGetWindowProperty(dpy, root, prop, 0L, sizeof(active), False, XA_WINDOW,
+        &type, &format, &len, &extra, &result) == Success && result) {
+            active = *(Window *)result;
+            XFree(result);
+            return active;
+    }
+    else
+        return None;
+}
 
 int main()
 {
@@ -10,5 +28,13 @@ int main()
     int screen = DefaultScreen(dpy);
     Window root = RootWindow(dpy, screen);
 
+    if(!(dpy = XOpenDisplay(NULL))) {
+        printf("simxcom: failed to open display");
+        exit(EXIT_FAILURE);
+    }
+
+    Window active_window = get_active_window(dpy, root);
+
+    XCloseDisplay(dpy);
     return EXIT_SUCCESS;
 }
