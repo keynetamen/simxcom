968464f594574f32bd32d0ec50c094259d11a099
Keyne Tamen <keynetamen@gmail.com>
2021-12-03 07:40:47 +0100

    Add get_inactive_windows()

--- src/simxcom.c
+++ src/simxcom.c
@@ -22,6 +22,34 @@ Window get_active_window(Display *dpy, Window root)
         return None;
 }
 
+Window *get_inactive_windows(Display *dpy, Window root, Window active_window,
+                            unsigned long *n_windows)
+{
+    Atom prop = XInternAtom(dpy, "_NET_CLIENT_LIST", True), type;
+    Window *client_windows, *inactive_windows;
+    int format;
+    unsigned long extra;
+    unsigned char *result = NULL;
+
+    if(XGetWindowProperty(dpy, root, prop, 0, 1024, False, XA_WINDOW, &type,
+        &format, n_windows, &extra, &result) == Success && result) {
+            client_windows = (Window *)result;
+            (*n_windows)--;
+            inactive_windows = (Window *)malloc(*n_windows * sizeof(Window));
+
+            for(int i = 0, j = 0; i < *n_windows + 1; i++) {
+                if(client_windows[i] != active_window) {
+                    inactive_windows[j] = client_windows[i];
+                    j++;
+                }
+            }
+            XFree(client_windows);
+            return inactive_windows;
+    }
+    else
+        return NULL;
+}
+
 int main()
 {
     Display *dpy = XOpenDisplay(NULL);
@@ -34,7 +62,11 @@ int main()
     }
 
     Window active_window = get_active_window(dpy, root);
+    int n_windows;
+    Window *inactive_windows = get_inactive_windows(dpy, root, active_window,
+        (unsigned long *)&n_windows);
 
+    free(inactive_windows);
     XCloseDisplay(dpy);
     return EXIT_SUCCESS;
 }
