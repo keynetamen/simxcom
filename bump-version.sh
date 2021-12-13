79b1123f83776c3f16e57e407f422bbf5cab303e
Keyne Tamen <keynetamen@gmail.com>
2021-12-13 17:31:18 +0100

    Remove bump-version.sh

--- bump-version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# != 1 ]; then
-    echo "Usage: $0 VERSION-NUMBER"
-else
-    sed -Ei "/project\(/s/VERSION ([0-9]\.*)+/VERSION $1/" CMakeLists.txt
-
-    echo "Files modified successfully, version bumped to $1"
-fi
